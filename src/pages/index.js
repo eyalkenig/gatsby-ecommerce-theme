05833228416d379987ef581c43e757e22ffc531d
Eyal Kenig <eyalkenig@gmail.com>
2022-10-13 15:18:42 +0300

    try to fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,6 +23,44 @@ const IndexPage = () => {
   const goToShop = () => {
     navigate('/shop');
   };
+  const checkStatus = (response) => {
+    if (response.status >= 200 && response.status < 300) {
+      return response
+    } else {
+      throw response.statusText
+    }
+  }
+  
+  const parseJSON = (response) => {
+    return response.json()
+  }
+  
+  const injectCustomerPortal = () => {
+    // fetch('/.netlify/functions/yotpo-auth')
+    fetch('https://courageous-frangollo-f4e785.netlify.app/.netlify/functions/yotpo-auth')
+    .then(checkStatus)
+    .then(parseJSON)
+    .then(json => {
+      console.log('jsn', json)
+      const placeholderParent = document.querySelector('.inject-here')
+      const placeholder = document.createElement('div')
+      placeholder.setAttribute('class', 'yotpo-widget-instance')
+      placeholder.setAttribute('data-yotpo-instance-id', '140312')
+      placeholder.setAttribute('widget-type', 'SubscriptionsCustomerPortal')
+      placeholder.setAttribute('anonymous', 'true')
+      placeholder.setAttribute('sent_at', encodeURIComponent(json.sentAt))
+      placeholder.setAttribute('customer_id', json.customerId)
+      placeholder.setAttribute('email', json.email)
+      placeholder.setAttribute('mode-headless', 'true')
+      placeholder.setAttribute('token', json.token)
+      placeholderParent.appendChild(placeholder)
+      const loader = document.createElement('script')
+      loader.setAttribute('src', 'https://cdn-widgetsrepository.yotpo.com/v1/loader/4zrQZ7EzIjvNNGYx1dGFMrTyrYYpSN5yibN70PGT')
+      loader.setAttribute('async', 'true')
+      document.head.appendChild(loader)
+    })
+  }
+  injectCustomerPortal()
 
   return (
     <Layout disablePaddingBottom>
